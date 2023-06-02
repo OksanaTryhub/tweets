72df0410e8ef3d61bab51bf6753cfb90181540e1
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-06-02 15:41:48 +0300

    updated

--- src/components/UserRoutes.jsx
+++ src/components/UserRoutes.jsx
@@ -4,16 +4,12 @@ import Tweets from '../pages/Tweets/Tweets';
 
 const UserRoutes = () => {
   return (
- 
- 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
- 
- 
-  );
+   );
 };
 
 export default UserRoutes;
