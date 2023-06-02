c958b5ec65de3b23702acc70babd46aab6bcf75a
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-06-02 16:12:38 +0300

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
-  const base = isProduction ? '/tweets/' : '/';
-
-  return {
-    plugins: [react()],
-    base: base
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/tweets-test/'
 })
