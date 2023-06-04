b51592c692314889535d51321e73cb02301c1ee5
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-06-04 14:17:28 +0300

    updated Filter

--- src/components/FIlter/Filter.jsx
+++ src/components/FIlter/Filter.jsx
@@ -1,28 +1,61 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './filter.module.css'
 
 const Filter = ({ onFilterChange }) => {
   const [isFilterVisible, setFilterVisible] = useState(false);
+  const [selectedValue, setSelectedValue] = useState('');
+  const filterRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   const handleClick = () => {
     setFilterVisible(prevState => !prevState);
   };
 
+  const handleClickOutside = (event) => {
+    if (filterRef.current && !filterRef.current.contains(event.target)) {
+      setFilterVisible(false);
+    }
+  };
+
   const handleOptionChange = (event) => {
-    const selectedValue = event.target.value;
-    onFilterChange(selectedValue);
+    const value = event.target.value;
+    onFilterChange(value);
+    setSelectedValue(value);
   };
 
   return (
-    <div>
+    <div ref={filterRef} className={styles.container} >
       <button onClick={handleClick} className={styles.filterBtn}>Filter</button>
       {isFilterVisible &&
-        <div className={styles.selectWrapper}>
-            <button value="show-all" onClick={handleOptionChange} className={styles.selectBtn}>Show All</button>
-            <button value="follow" onClick={handleOptionChange} className={styles.selectBtn}>Follow</button>
-            <button value="followings" onClick={handleOptionChange} className={styles.selectBtn}>Followings</button>
+        <div className={styles.selectWrapper }>
+            <button 
+            value="show-all" 
+            onClick={handleOptionChange} 
+            className={`${styles.selectBtn} ${selectedValue === 'show-all' ? styles.active : ''}`}
+            >
+              Show All
+            </button>
+            <button 
+            value="follow" 
+            onClick={handleOptionChange} 
+            className={`${styles.selectBtn} ${selectedValue === 'follow' ? styles.active : ''}`}
+            >
+              Follow
+            </button>
+          <button
+            value="followings"
+            onClick={handleOptionChange}
+            className={`${styles.selectBtn} ${selectedValue === 'followings' ? styles.active : ''}`}
+          >
+            Followings</button>
         </div>
       }
     </div>
