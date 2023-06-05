cf8b3a2e40607cfd3104d9e5a4b97f6c69a4dafb
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-06-05 12:56:28 +0300

    updated UserList

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -2,27 +2,34 @@ import PropTypes from "prop-types";
 
 import UserCard from "../UserCard/UserCard";
 import styles from "./userList.module.css";
+import Loader from "../Loader/Loader";
 
 const UserList = ({ users, loading, page, handleUnfollowClick, handleFollowClick }) => {
   return (
     <div className={styles.userList}>
-      {!users.length && !loading && <p>There are no tweets yet</p>}
-      {users.length &&
-        !loading &&
-        users
-          .slice(0, page * 3)
-          .map(({ id, user, tweets, followers, avatar }) => (
-            <UserCard
-              key={id}
-              id={id}
-              user={user}
-              tweets={tweets}
-              followers={followers}
-              avatar={avatar}
-              handleUnfollowClick={handleUnfollowClick}
-              handleFollowClick={handleFollowClick}
-            />
-          ))}
+     {users.length && !loading ? (
+  users
+    .slice(0, page * 3)
+    .map(({ id, user, tweets, followers, avatar }) => (
+      <UserCard
+        key={id}
+        id={id}
+        user={user}
+        tweets={tweets}
+        followers={followers}
+        avatar={avatar}
+        handleUnfollowClick={handleUnfollowClick}
+        handleFollowClick={handleFollowClick}
+      />
+    ))
+) : (
+  !users.length && !loading && 
+  <div className={styles.textWrapper}>
+    <p className={styles.text}>There are no tweets yet</p>
+    <Loader/>
+  </div>
+)}
+
     </div>
   );
 };
